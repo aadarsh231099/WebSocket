9a40cf4340cd878e9805b81b5d88c314f2c5bb5e
Aadarsh <aadarshhanu23@gmail.com>
2023-02-16 19:46:35 +0530

    added secureServer file and key+cert

---
+++ secureServer.js
@@ -0,0 +1,40 @@
+const WebSocket = require('ws');
+const https = require('https');
+const fs = require('fs');
+
+const server = https.createServer({
+  cert: fs.readFileSync('C:\\Users\\91808\\Desktop\\Numo\\WebSocket\\cert.pem'),
+  key: fs.readFileSync('C:\\Users\\91808\\Desktop\\Numo\\WebSocket\\key.pem')
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    const json = JSON.parse(message);
+
+    const response = [
+      2,
+      json[1],
+      {
+        status: 'Accepted',
+        currentTime: new Date().toISOString(),
+        heartbeatInterval: 300,
+      },
+    ];
+
+    ws.send(JSON.stringify(response));
+    console.log(`Sent message: ${JSON.stringify(response)}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
